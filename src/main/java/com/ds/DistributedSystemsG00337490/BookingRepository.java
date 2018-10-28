e808c007cd5c506713145e2af00e32c25ddd4b54
Edward Eldridge <steadyeddie101@hotmail.co.uk>
2018-10-28 15:54:50 +0000

    Added a GET request method to handle bookings with a specific ID

--- src/main/java/com/ds/DistributedSystemsG00337490/BookingRepository.java
+++ src/main/java/com/ds/DistributedSystemsG00337490/BookingRepository.java
@@ -1,5 +1,55 @@
 package com.ds.DistributedSystemsG00337490;
 
-public class BookingRepository {
+import java.util.ArrayList;
+import java.util.List;
 
+public class BookingRepository 
+{	
+	// Variables
+	List<Booking> bookings;
+	
+	public BookingRepository() 
+	{
+		bookings = new ArrayList<>();
+
+		Booking booking1 = new Booking();
+		booking1.setOrderID(21);
+		booking1.setFirstName("Eddie");
+		booking1.setLastName("Eldridge");
+		booking1.setOrderCost(200.30);
+		booking1.setCarName("Golf");
+		booking1.setCarRegistration("18-07-1992");
+
+		Booking booking2 = new Booking();
+		booking2.setOrderID(20);
+		booking2.setFirstName("Luke");
+		booking2.setLastName("Haliday");
+		booking2.setOrderCost(200.30);
+		booking2.setCarName("Golf");
+		booking2.setCarRegistration("18-07-1992");
+
+		bookings.add(booking1);
+		bookings.add(booking2);
+
+	}
+
+	public List<Booking> getBookings() 
+	{
+		return bookings;
+	}
+
+	public Booking getBooking(int orderID) 
+	{
+		
+		for(Booking booking: bookings)
+		{
+			if(booking.getOrderID() == orderID)
+			{
+				System.out.println("Booking found!");
+				return booking;
+			}
+		}
+		
+		return null; 
+	}
 }
--- src/main/java/com/ds/DistributedSystemsG00337490/BookingResource.java
+++ src/main/java/com/ds/DistributedSystemsG00337490/BookingResource.java
@@ -12,30 +12,28 @@ import javax.ws.rs.GET;
 @Path("bookings")
 public class BookingResource 
 {
+	// Variables
+	BookingRepository repo = new BookingRepository();
+	
 	
 	@GET
 	@Produces(MediaType.APPLICATION_XML)
 	public List<Booking> getBookings() 
 	{
-		System.out.println("Inside getBooking!");
-		
-		Booking booking1 = new Booking();
-		booking1.setFirstName("Eddie");
-		booking1.setLastName("Eldridge");
-		booking1.setOrderCost(200.30);
-		booking1.setOrderID(20);
-		booking1.setCarName("Golf");
-		booking1.setCarRegistration("18-07-1992");
-		
-		Booking booking2 = new Booking();
-		booking2.setFirstName("Luke");
-		booking2.setLastName("Haliday");
-		booking2.setOrderCost(200.30);
-		booking2.setOrderID(20);
-		booking2.setCarName("Golf");
-		booking2.setCarRegistration("18-07-1992");
-		
-		List<Booking> bookings = Arrays.asList(booking1,booking2);
-		return bookings;
+		System.out.println("Inside getBookings!");
+		return repo.getBookings();
 	}
+	
+	/*
+	@GET
+	@Produces(MediaType.APPLICATION_XML)
+	public Booking getBooking() 
+	{
+		System.out.println("Inside getBooking!");
+		return repo.getBooking(20);
+	}
+	*/
+	
 }
+
+
