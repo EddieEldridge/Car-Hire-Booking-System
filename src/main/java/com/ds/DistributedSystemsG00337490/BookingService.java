e87de5fd03bd3ca4bd56f541666ba36c747595c3
Edward Eldridge <steadyeddie101@hotmail.co.uk>
2018-10-29 13:22:00 +0000

    Added BookingService server

--- src/main/java/com/ds/DistributedSystemsG00337490/BookingService.java
+++ src/main/java/com/ds/DistributedSystemsG00337490/BookingService.java
@@ -1,6 +1,30 @@
 package com.ds.DistributedSystemsG00337490;
 
+import java.rmi.Naming;
+import java.rmi.registry.LocateRegistry;
+
+// Booking Service
+// This class serves as the gateway for our booking client to 
 public class BookingService
 {
-
+	public static void main(String[] args) throws Exception
+	{
+		// Create instance of bookingService implementation
+		BookingServiceImpl bookingService = new BookingServiceImpl();
+		
+		// Start the RMI Registry 
+		LocateRegistry.createRegistry(1099);
+	}
+	
+	// Bind the bookingService object to the registry 
+	try
+	{
+		Naming.rebind("bookingService", bookingService);
+		System.out.println("Server started...");
+	}
+	catch(Exception e)
+	{
+		System.out.println("Server error: " + e);
+	}
+	
 }
