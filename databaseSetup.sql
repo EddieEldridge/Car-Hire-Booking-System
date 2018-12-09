3113bb38e637177c28991f3fcdd395e8e499e3bc
Edward Eldridge <steadyeddie101@hotmail.co.uk>
2018-12-09 15:29:16 +0000

    Vehicle schema and db script setup

--- databaseSetup.sql
+++ databaseSetup.sql
@@ -16,15 +16,15 @@ CREATE TABLE Booking
     customerID INT NOT NULL PRIMARY KEY,
     bookingStartDate VARCHAR(50) NOT NULL,
     bookingEndDate VARCHAR(50) NOT NULL,
-    vehicleRegBooked VARCHAR(50) NOT NULL
+    carRegistration INT(50) NOT NULL
 );
 
 CREATE TABLE Vehicle
 (
-    customerID INT NOT NULL PRIMARY KEY,
-    firstName VARCHAR(50) NOT NULL,
-    lastName VARCHAR(50) NOT NULL,
-    insuranceID INT(50) NOT NULL,
-    customerAdrress VARCHAR(50) NOT NULL
+    carRegistration INT NOT NULL PRIMARY KEY,
+    carManufacturer VARCHAR(50) NOT NULL,
+    carModel VARCHAR(50) NOT NULL,
+    carColour VARCHAR(50) NOT NULL,
+    carEngineSize INT(50) NOT NULL
 );
 GO
