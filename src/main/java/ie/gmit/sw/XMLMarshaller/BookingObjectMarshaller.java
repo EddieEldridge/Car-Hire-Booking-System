8a5a2e814c9ea2b89252c95fb4832aba06e6b3da
Edward Eldridge <steadyeddie101@hotmail.co.uk>
2018-12-09 16:54:19 +0000

    Set up marshaller and unmarshaller for objects and XML

--- src/main/java/ie/gmit/sw/XMLMarshaller/BookingObjectMarshaller.java
+++ src/main/java/ie/gmit/sw/XMLMarshaller/BookingObjectMarshaller.java
@@ -1,13 +1,16 @@
 package ie.gmit.sw.XMLMarshaller;
 
+import java.io.StringReader;
 import java.io.StringWriter;
 
 import ie.gmit.sw.BookingModel.Booking;
 
 import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBElement;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
+import javax.xml.transform.stream.StreamSource;
 
 public class BookingObjectMarshaller
 {
@@ -16,6 +19,7 @@ public class BookingObjectMarshaller
 		
 	}
 	
+	// Function to marshal the Java object(Booking) as XML
 	protected String marshalBooking(Booking booking)
 	{
 		StringWriter stringWriter = new StringWriter();
@@ -25,7 +29,11 @@ public class BookingObjectMarshaller
 		try
 		{
 			JAXBContext jaxb = JAXBContext.newInstance("ie.gmit.sw.BookingModel");
+			
+			// Create the unmarshaller
 			xmlMarshaller = jaxb.createMarshaller();
+			
+			// Marshall the booking object as XML
 			xmlMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
 			xmlMarshaller.marshal(booking, stringWriter);
 		} 
@@ -33,7 +41,41 @@ public class BookingObjectMarshaller
 		{
 			System.out.println("Error marshalling booking as XML: " + e);
 		}
-				
+		
+		// Return the XML generated from the object as a string using our stringWriter
 		return stringWriter.toString();
+	}
+	
+	// Function to unmarshal the XML to a Java object(Booking)
+	protected Booking unmarshalBooking(String xml)
+	{
+		StringReader stringReader = new StringReader(xml);
+		Unmarshaller xmlUnmarshaller;
+		
+		Booking bookingToUnmarshal = null;
+		
+		// Try and unmarshal the booking
+		try
+		{
+			JAXBContext jaxb = JAXBContext.newInstance("ie.gmit.sw.BookingModel");
+			
+			// Create the unmarshaller
+			xmlUnmarshaller = jaxb.createUnmarshaller();
+			
+			// Create a source to read in our XML to
+			StreamSource src = new StreamSource(stringReader);
+			
+			// Using this source of XML, unmarshal the xml to a Booking object
+			JAXBElement<Booking> bookingXMLElement = xmlUnmarshaller.unmarshal(src, Booking.class);
+			bookingToUnmarshal = (Booking) bookingXMLElement.getValue();
+			
+		} 
+		catch (JAXBException e)
+		{
+			System.out.println("Error unmarshalling booking as XML: " + e);
+		}
+				
+		// Return the booking object we just unmarshalled
+		return bookingToUnmarshal;
 	}
 }
